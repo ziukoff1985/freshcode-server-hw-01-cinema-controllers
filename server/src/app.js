029b97733530482cfccad8ccc97cdc095c8e02c7
Borys Ziukov <ziukoff1985@gmail.com>
2026-03-23 13:19:14 +0200

    Add db.js

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('This is the home page!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
